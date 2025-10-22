83cd918f71b9f49e2ae513f705ceea3c40e1486e
Arthur Telles <arthurtelles19@gmail.com>
2025-10-21 21:40:59 -0300

    feat: solicitacao de carona

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
-import RegisterRide from '../pages/registerRide';
+import RegisterRideRequestPage from '../pages/registerRideRequest';
 
 export default function Index() {
     return(
         <View style={{flex:1}}>
             {/* <Login /> */}
             {/* <RegisterUser /> */}
-            <RegisterRide />
+            {/* <RegisterRide /> */}
+            <RegisterRideRequestPage />
         </View>
     )
 }
